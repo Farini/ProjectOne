3d5275c07abf2b3b940c8448df8d396306a56c8c
Farini <bsme@marlones.com>
2019-12-24 17:22:11 -0500

    Added file and function

---
+++ Sources/ProjectOne/Greeter.swift
@@ -0,0 +1,12 @@
+//
+//  Greeter.swift
+//  ProjectOne
+//
+//  Created by Farini on 12/24/19.
+//
+
+import Foundation
+
+func sayHello(name: String) {
+    print("Hello, \(name)!")
+}
--- Sources/ProjectOne/main.swift
+++ Sources/ProjectOne/main.swift
@@ -1,2 +1,36 @@
 print("Hello, Farini!")
 print("How are you, friend?")
+
+#if os(Linux)
+
+// Code specific to Linux
+print("Linux also says Hello")
+
+#elseif os(macOS)
+
+// Code specific to macOS
+print("Mac also says Hello")
+
+#endif
+
+#if canImport(UIKit)
+
+// Code specific to platforms where UIKit is available
+print("UIKit also says Hello")
+
+#endif
+
+#if canImport(Foundation)
+
+print("Foundation also says Hello")
+// Code specific to platforms where Foundation is available
+
+#endif
+
+
+if CommandLine.arguments.count != 2 {
+    print("Usage: hello NAME")
+} else {
+    let name = CommandLine.arguments[1]
+    sayHello(name: name)
+}
